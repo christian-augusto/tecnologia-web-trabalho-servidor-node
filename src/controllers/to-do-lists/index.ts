234182754ef9b9ce32cdb47355431104666ecb3a
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-09-07 23:43:15 -0300

    refactor: to-do-lists controller

--- src/controllers/to-do-lists/index.ts
+++ src/controllers/to-do-lists/index.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import ICreateToDoListUsecase from "@usecases/contracts/icreate-to-do-list-usecase";
 import IListToDoListsUsecase from "@usecases/contracts/ilist-to-do-lists-usecase";
 import IDeleteToDoListUsecase from "@usecases/contracts/idelete-to-do-list-usecase";
+import DeleteToDoRequest from "./requests/delete-to-do-list-request";
 import DeleteToDoListInput from "@usecases/ports/input/delete-to-do-list-input";
 
 export default function (
@@ -24,12 +25,8 @@ export default function (
     res.status(201).json(responseBody);
   });
 
-  router.delete("/:id", (req: Request, res: Response) => {
-    const deleteToDoListInput: DeleteToDoListInput = {
-      id: Number(req.params.id),
-    };
-
-    deleteToDoListUsecase.execute(deleteToDoListInput);
+  router.delete("/:id", (req: DeleteToDoRequest<DeleteToDoListInput>, res: Response) => {
+    deleteToDoListUsecase.execute(req.params);
 
     res.status(204).end();
   });
